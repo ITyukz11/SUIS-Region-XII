3e08f49066b2c25038a813c069bb3891ad598e23
Errol Robyn M. Abella <it.era0211@gmail.com>
2023-10-06 12:28:10 +0800

    test

--- pages/ess/3a.js
+++ pages/ess/3a.js
@@ -317,13 +317,13 @@ if(batchUploadStatus){
 
  
   //Counting Sequence Number Function
-  const countSequenceNo = async (datas) => {
+  const countSequenceNo = (datas) => {
     if (!Array.isArray(datas) || datas.length === 0) {
       return  <div role="status" className="max-w-sm animate-pulse" >
                 <div className="h-4 bg-gray-200 rounded-md dark:bg-gray-700 w-8 mb-4"></div>
               </div>;
     }
-
+    
     const seqNoCount = datas.reduce((count, item) => {
       // Check if the item has a SeqNo property
       if (item['Collective CLOA Sequence Number']) {
